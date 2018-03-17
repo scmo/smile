f026864a82a7de811adf2927e3de05fb6cb58921
joelsonderegger <joel.sonderegger@googlemail.com>
2018-03-17 14:25:44 +0100

    improve webcam quality

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const https = require('https');
 const axios = require('axios')
 var cloudinary = require('cloudinary')
 const bodyParser = require('body-parser');
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 var config = require('./config.js');
 
